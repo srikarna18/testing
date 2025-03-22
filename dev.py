12d097cce20e2031e0fc619167fe333497631a92
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-03-22 09:22:02 +0530

    Update dev.py

--- dev.py
+++ dev.py
@@ -1,7 +1,8 @@
-# function with two arguments
+# Function with two arguments
 def add_numbers(num1, num2):
-    sum = num1 + num2
-    print("Sum: ", sum)
+    """Returns the sum of two numbers."""
+    result = num1 + num2
+    print("Sum:", result)
 
-# function call with two values
+# Function call with two values
 add_numbers(5, 4)
