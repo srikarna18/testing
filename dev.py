0855a2a17634cf00341a1feb2c23ab41583b1ca7
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-03-22 09:23:18 +0530

    Update dev.py

--- dev.py
+++ dev.py
@@ -1,4 +1,5 @@
 # Function with two arguments
+"""This module contains functions for mathematical operations."""
 def add_numbers(num1, num2):
     """Returns the sum of two numbers."""
     result = num1 + num2
